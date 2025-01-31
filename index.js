d0903f03479021b694219bcb9b0ed8a1ca0e91d9
ManojGowda2006 <manojgowdan807@gmail.com>
2025-01-31 10:10:08 +0530

    completed the task

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const { resolve } = require('path');
-
+const User  = require("./schema");
 const app = express();
 const port = 3010;
+const mongoose = require("mongoose");
+require('dotenv').config()
+
+
+mongoose.connect(process.env.db_URL)
+.then(console.log("Connected to Database"))
+.catch((err) => console.log("Error connecting to database"));
 
 app.use(express.static('static'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
@@ -13,3 +21,22 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+app.post("/api/users", async(req,res) => {
+  try{
+  const {name,email,password} = req.body;
+  
+  if(!name || !email || !password){
+    res.status(400).send("All fields are required");
+  }
+  const newUser = new User({name,email,password})
+  await newUser.save();
+
+  console.log(newUser);
+  }
+  catch{
+    res.status(500).send("Internal server error");
+  }
+  res.status(201).send("data stored successfully");
+  
+})
